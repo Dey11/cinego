8b601b0f7928433dbb403d5ff587d5432879758f
Dey11 <shreyandey11@gmail.com>
2024-11-06 11:49:33 +0530

    add constant for the providers

---
+++ src/lib/constants.ts
@@ -0,0 +1,179 @@
+import { DEFAULT_MOVIE_PROVIDER, DEFAULT_TV_PROVIDER } from "./utils";
+
+export const PROVIDERS_TV = [
+  DEFAULT_TV_PROVIDER,
+  {
+    name: "Orion",
+    url: "https://embed.su/embed/tv/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Duke",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Echo",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Asia",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/IN/flat/24.png`,
+  },
+  {
+    name: "Bravo",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Gama",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/IN/flat/24.png`,
+  },
+  {
+    name: "Atlas",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Golf",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Pulse",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Alpha",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Delta",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Lima",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Nova",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Jade",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/PT/flat/24.png`,
+  },
+  {
+    name: "French",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/FR/flat/24.png`,
+  },
+  {
+    name: "Spanish",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/ES/flat/24.png`,
+  },
+  {
+    name: "Viet",
+    url: "https://vidsrc.xyz/embed/tv/",
+    countryUrl: `https://flagsapi.com/VN/flat/24.png`,
+  },
+];
+
+export const PROVIDERS_MOVIE = [
+  DEFAULT_MOVIE_PROVIDER,
+  {
+    name: "Orion",
+    url: "https://embed.su/embed/movie/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Duke",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Echo",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Asia",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/IN/flat/24.png`,
+  },
+  {
+    name: "Bravo",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Gama",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/IN/flat/24.png`,
+  },
+  {
+    name: "Atlas",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Golf",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Pulse",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Alpha",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Delta",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Lima",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/US/flat/24.png`,
+  },
+  {
+    name: "Nova",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/GB/flat/24.png`,
+  },
+  {
+    name: "Jade",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/PT/flat/24.png`,
+  },
+  {
+    name: "French",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/FR/flat/24.png`,
+  },
+  {
+    name: "Spanish",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/ES/flat/24.png`,
+  },
+  {
+    name: "Viet",
+    url: "https://vidsrc.xyz/embed/movie/",
+    countryUrl: `https://flagsapi.com/VN/flat/24.png`,
+  },
+];
