63604e33e2eb100f7a48dc5535809cd6b702e466
Dey11 <shreyandey11@gmail.com>
2024-11-25 01:36:21 +0530

    fix hydration issue

--- src/components/footer/mobile-nav.tsx
+++ src/components/footer/mobile-nav.tsx
@@ -31,7 +31,7 @@ export default function MobileNav() {
       <nav className="fixed bottom-0 left-0 right-0 z-50 bg-gray-900/70 px-4 py-2 md:hidden">
         <ul className="flex items-center justify-between">
           <li>
-            <Link href="/home" className="flex flex-col items-center">
+            <Link href="/" className="flex flex-col items-center">
               <Home className="h-6 w-6 text-red-500" />
               <span className="mt-1 text-xs text-gray-400">Home</span>
             </Link>
