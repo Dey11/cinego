979603cfd81edb16dd693c05a0cbdfebd5fec753
Dey11 <shreyandey11@gmail.com>
2024-12-04 16:29:27 +0530

    fix

--- src/lib/api-calls/anime.ts
+++ src/lib/api-calls/anime.ts
@@ -1,13 +1,8 @@
-// import { ANIME } from "@consumet/extensions";
-
-// const anilist = new ANIME.Anilist();
-
 export const fetchAnimeInfo = async (id: string) => {
   try {
-    // https://api-consumet-org-beige.vercel.app/meta/anilist/info/
     const info = await fetch(
-      //   `https://api-consumet-org-beige.vercel.app/meta/anilist/info/${id}`,
-      `http://0.0.0.0:5000/meta/anilist/info/${id}`,
+      `${process.env.ANIME_API}/meta/anilist/info/${id}`,
+      //   `http://0.0.0.0:5000/meta/anilist/info/${id}`,
     ).then((res) => res.json());
     // console.log(id, info);
     // console.log(info);
