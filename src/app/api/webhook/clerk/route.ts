e3e6cba5884a6ed4269f0c23c9cf58db8138c0aa
Dey11 <shreyandey11@gmail.com>
2024-12-04 10:20:21 +0530

    fix webhook endpoint

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -4,7 +4,20 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/db";
 
 export async function POST(req: Request) {
-  // Get the headers
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+
+  if (!SIGNING_SECRET) {
+    throw new Error(
+      "Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local",
+    );
+  }
+
+  console.log("Received webhookkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk:");
+
+  // Create new Svix instance with secret
+  const wh = new Webhook(SIGNING_SECRET);
+
+  // Get headers
   const headerPayload = await headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
@@ -12,21 +25,16 @@ export async function POST(req: Request) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    return new Response("Error: Missing Svix headers", {
       status: 400,
     });
   }
-
-  // Get the body
+  // Get body
   const payload = await req.json();
   const body = JSON.stringify(payload);
-
-  // Create a new Svix instance with your webhook secret
-  const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET || "");
-
   let evt: WebhookEvent;
 
-  // Verify the webhook
+  // Verify payload with headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -34,14 +42,20 @@ export async function POST(req: Request) {
       "svix-signature": svix_signature,
     }) as WebhookEvent;
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    console.error("Error: Could not verify webhook:", err);
+    return new Response("Error: Verification error", {
       status: 400,
     });
   }
 
+  // Do something with payload
+  // For this guide, log payload to console
+  const { id } = evt.data;
+
   // Handle the webhook
   const eventType = evt.type;
+
+  // console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
 
   if (eventType === "user.created" || eventType === "user.updated") {
     const { id, email_addresses, image_url, first_name, last_name } = evt.data;
